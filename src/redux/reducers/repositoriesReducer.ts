91058529ec7d4ace8d3d360d974304f2af35307f
kiranaz <kiranaz_123@outlook.com>
2022-02-24 23:11:12 +0500

    Update repositoriesReducer.ts

--- src/redux/reducers/repositoriesReducer.ts
+++ src/redux/reducers/repositoriesReducer.ts
@@ -4,18 +4,35 @@ interface RepositoriesState {
     data: string[]
 }
 
-interface Action{
-    type: string,
-    payload?: any
+interface SearchRepoAction{
+    type: ActionTypes.SEARCH_REPO,
+}
+
+interface SearchRepoSuccessAction{
+    type: ActionTypes.SEARCH_REPO_SUCCESS,
+    payload: string[]
+}
+
+interface SearchRepoErrorAction{
+    type: ActionTypes.SEARCH_REPO_ERROR,
+    payload: string
+}
+
+type Action = SearchRepoAction | SearchRepoSuccessAction | SearchRepoErrorAction;
+
+enum ActionTypes {
+    SEARCH_REPO = 'search_repo',
+    SEARCH_REPO_SUCCESS = 'search_repo_success',
+    SEARCH_REPO_ERROR = 'search_repo_error'
 }
 
 const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
     switch (action.type) {
-        case 'search_repo':
+        case ActionTypes.SEARCH_REPO:
             return { loading: true, error: null, data: [] }
-        case 'search_repo_success':
+        case ActionTypes.SEARCH_REPO_SUCCESS:
             return { loading: false, error: null, data: action.payload }
-        case 'search_repo_error':
+        case ActionTypes.SEARCH_REPO_ERROR:
             return { loading: false, error: action.payload, data: [] }
         default:
             return state;
