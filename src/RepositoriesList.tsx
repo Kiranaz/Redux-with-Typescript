bdd0eb40e3243b3933150d891054710f07253d2a
kiranaz <kiranaz_123@outlook.com>
2022-02-25 11:30:55 +0500

    Adding useActions

--- src/RepositoriesList.tsx
+++ src/RepositoriesList.tsx
@@ -1,13 +1,16 @@
-import { useState } from "react"
-import { useDispatch } from "react-redux";
-import { actionCreators } from "./redux";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { useActions } from "./hooks/useActions";
+
 export const RepositoriesList: React.FC = () => {
     const [term, setTerm] = useState('');
-    const Dispatch = useDispatch();
-
+    const { searchRepositories } = useActions(); //destructuring searchRepositories from useActions 
+    const {data, error, loading} = useSelector((state: any) => state.repositories)
+    console.log(data);
     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event?.preventDefault();
-        Dispatch(actionCreators.searchRepositories(term))
+        //         Dispatch(actionCreators.searchRepositories(term));
+        searchRepositories(term);
     }
 
     return (
