bb88048b49b35459d75eae962c65d72baef21a37
kiranaz <kiranaz_123@outlook.com>
2022-02-25 11:04:23 +0500

    Update RepositoriesList.tsx

--- src/RepositoriesList.tsx
+++ src/RepositoriesList.tsx
@@ -1,10 +1,13 @@
 import { useState } from "react"
-
+import { useDispatch } from "react-redux";
+import { actionCreators } from "./redux";
 export const RepositoriesList: React.FC = () => {
     const [term, setTerm] = useState('');
+    const Dispatch = useDispatch();
 
     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event?.preventDefault();
+        Dispatch(actionCreators.searchRepositories(term))
     }
 
     return (
